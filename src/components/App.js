b41528387282162f25c288859e72aa50b671d8ba
AndreyB <AnB.124@yandex.ru>
2022-04-30 14:38:19 +0700

    основа

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src className="App-logo" alt="logo" />
+        <p>
+          КИКИрики.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          ХобаНА
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
